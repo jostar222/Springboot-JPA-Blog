153ac2b5145c7602ee2949798c061fc4139ac7bc
jostar222 <whtjdgus911@gmail.com>
2024-01-21 17:53:57 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+package com.cos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
+
+// exclude 옵션 추가(DB정보 없음)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
