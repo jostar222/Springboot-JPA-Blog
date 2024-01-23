3b21900560c4ab947841d1cb3ca5366cedd346ce
jostar222 <whtjdgus911@gmail.com>
2024-01-24 00:04:59 +0900

    더티 체킹 및 업데이트 처리(@Transactional)

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,45 @@ public class DummyControllerTest {
 
     @Autowired // DummyControllerTest가 메모리에 뜰 때 UserRepository도 메모리에 같이 뜬다. (의존성주입=DI(Dependency Injection)
     private UserRepository userRepository;
+
+    // save함수는 id를 전달하지 않으면 insert를 해주고
+    // save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+    // save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 한다.
+    // email, password
+    @Transactional // 함수 종료시에 자동 commit이 됨.
+    @PutMapping("/dummy/user/{id}") // 경로 같아도 Put, Get 메서드 알아서 구분해준다.
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // json 데이터를 요청 => Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아줌
+        System.out.println("id = " + id);
+        System.out.println("password = " + requestUser.getPassword());
+        System.out.println("email = " + requestUser.getEmail());
+
+        // save() 사용 시 코드
+        // user 객체를 받아와서 변경할 컬럼만 set해주고 save() 한다. 가져온 user 객체는 null 값이 없으므로 null이 들어가지 않음.
+
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        // userRepository.save(user);
+
+        // 더티 체킹?
+        /*
+        여기에서 Dirty란 상태의 변화가 생긴 정도로 이해하시면 됩니다.
+        즉, Dirty Checking이란 상태 변경 검사 입니다.
+        JPA에서는 트랜잭션이 끝나는 시점에 변화가 있는 모든 엔티티 객체를 데이터베이스에 자동으로 반영해줍니다.
+        이때 변화가 있다의 기준은 최초 조회 상태입니다.
+        당연히 이런 상태 변경 검사의 대상은 영속성 컨텍스트가 관리하는 엔티티에만 적용됩니다.
+           - detach된 엔티티 (준영속)
+           - DB에 반영되기 전 처음 생성된 엔티티 (비영속)
+        준영속/비영속 상태의 엔티티는 Dirty Checking 대상에 포함되지 않습니다.
+        즉, 값을 변경해도 데이터베이스에 반영되지 않는다는 것이죠.
+        */
+
+       return null;
+    }
+
 
     // http://localhost:8000/blog/dummy/user
     @GetMapping("/dummy/users")
